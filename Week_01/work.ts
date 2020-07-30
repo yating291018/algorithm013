34bf8e03f9b211f4c22a73d188b41cfeeceb74eb
hugang1 <hugang1@tuhu.cn>
2020-07-30 20:27:28 +0800

    作业doing

---
+++ Week_01/work.ts
@@ -0,0 +1,110 @@
+// 作业
+
+/**
+  1. 用 add first 或 add last 这套新的 API 改写 Deque 的代码
+ */
+
+
+ /*
+ ** 2. 分析 Queue 和 Priority Queue 的源码
+  */
+
+
+/***
+3. 删除排序数组中的重复项
+   利用指针，比较两个相邻的元素，如果相等，就进行下一个比较，如果不相等就把当前元素加入新指针为下标的
+ */
+
+function removeDuplicates(nums: number[]): number {
+  let j = 0
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== nums[i + 1]) {
+          nums[j] = nums[i]
+          j++
+      }
+  }
+  return j
+};
+
+// 利用数组自已的删除方法
+function removeDuplicates(nums: number[]): number {
+  for (let i = 0;i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+        nums.splice(i, 1)
+        i--
+    }
+  }
+  return nums.length
+}
+
+
+/***
+  4. 旋转数组
+ */
+ function rotate(nums: number[], k: number): void {
+  let temp = 0
+  for (let i = 0; i < k; i++) {
+      let prev = nums[nums.length - 1]
+      for (let j = 0; j < nums.length; j++) {
+          temp = nums[j]
+          nums[j] = prev
+          prev = temp
+      }
+  }
+};
+
+/***
+5. 加一
+ */
+function plusOne(digits: number[]): number[] {
+  let temp = 1
+  if (digits.length === 1) {
+      if (digits[0] + 1 === 10) {
+          return [1, 0]
+      }
+  }
+  for (var i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] + temp === 10) {
+          digits[i] = 0
+          temp = 1
+      } else {
+          digits[i] += temp
+          temp = 0
+      }
+  }
+
+  if (i < 0) {
+      if (temp === 1) {
+          digits.unshift(1)
+      }
+  }
+  return digits
+};
+
+/****
+  合并两个有序链表
+  链表的移动， p = p.next while。 p === null为止
+ */
+ function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  if (l1 === null && l2 === null) {
+      return null
+  }
+  let first = l1,
+      second = l2,
+      p = null
+  let newnode = new ListNode()
+  p = newnode
+  while (first !== null && second !== null) {
+      newnode.next = first.val <= second.val ? first : second
+      newnode = newnode.next
+      first.val <= second.val ? first = first.next : second = second.next
+  }
+  if (first !== null) {
+      newnode.next = first
+  }
+  if (second !== null) {
+      newnode.next = second
+  }
+  // newnode = newnode.next
+  return p.next
+};
